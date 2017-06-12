1e330f14d4e28cc3ef574f48daf71de121e5e32a
dsoraluz <d.soraluz@gmail.com>
2017-06-11 21:16:52 -0400

    added create-admin-seed.js

---
+++ bin/create-admin-seed.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const User = require("../models/user-model.js");
+
+const password = process.env.ADMIN_PASSWORD;
+
+//Encypt password
+const salt = bcrypt.genSaltSync(10);
+const hashPass = bcrypt.hashSync(password, salt);
+
+const user = {
+  firstName: "Roberto",
+  lastName: "Figueredo",
+  username: "admin@newsoul.com",
+  encryptedPassword: hashPass,
+  phone: "305-595-5959",
+  address: {
+    street: "1596 W Flagler St.",
+    city: "Miami",
+    state: "Florida",
+    zip: "33136"
+  },
+  language: "English",
+  referredBy: "",
+  role: "ADMIN",
+  descendedFrom:'',
+  descendants:[],
+  points: 0
+};
+
+User.create(user, (err, docs)=>{
+  if (err){
+    throw(err);
+  }
+  docs.forEach((doc)=>{
+    console.log(`${docs._id} ${docs.firstName} ${docs.lastName}`);
+  });
+
+  mongoose.disconnect();
+
+});
